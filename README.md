035f9a50fe20539200cb7359116fa60c57ef74b4
marcussoliva <marcus.soliva@volanty.com>
2021-04-08 17:33:40 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# web-scrapping
-This is a web scrapping example
+# Quotes Crawler
+
+A web crawling and web scraping example with [Scrapy](http://quotes.toscrape.com/).
+
+## Configuration
+
+After clone this project, install [pip](https://pip.pypa.io/en/stable/) and packages.
+
+```bash
+pip install -r requirements.txt
+```
+
+
+## Run crawler 
+
+```bash
+scrapy crawl home_page
+```
---
+++ quotescrawler/spiders/home_page.py
@@ -0,0 +1,25 @@
+import scrapy
+
+from scrapy.loader import ItemLoader
+from quotescrawler.items import QuoteItem
+
+
+class QuoteSpider(scrapy.Spider):
+    name = "home_page"
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+
+        quotes = []
+        for quote in response.xpath('/html/body/div/div[2]/div[1]/div'):
+
+            item_loader = ItemLoader(item=QuoteItem(), response=response)
+            item_loader.add_value('text', quote.xpath('.//span[1]/text()').get())
+            item_loader.add_value('author', quote.xpath('.//span[2]/small/text()').get())
+
+            item_loaded = item_loader.load_item()
+
+            quotes.append(item_loaded)
+
+        self.log(quotes)
