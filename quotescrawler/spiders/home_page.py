b101c5d95bcfe94eea38d59c01d64cf46b8fda26
Marcus Soliva <solivavinicius@gmail.com>
2021-04-24 19:12:38 -0300

    Update home_page.py

--- quotescrawler/spiders/home_page.py
+++ quotescrawler/spiders/home_page.py
@@ -14,9 +14,9 @@ class QuoteSpider(scrapy.Spider):
         quotes = []
         for quote in response.xpath('/html/body/div/div[2]/div[1]/div'):
 
-            item_loader = ItemLoader(item=QuoteItem(), response=response)
-            item_loader.add_value('text', quote.xpath('.//span[1]/text()').get())
-            item_loader.add_value('author', quote.xpath('.//span[2]/small/text()').get())
+            item_loaded = ItemLoader(item=QuoteItem(), response=response)
+            item_loaded.add_value('text', quote.xpath('.//span[1]/text()').get())
+            item_loaded.add_value('author', quote.xpath('.//span[2]/small/text()').get())
 
             item_loaded = item_loader.load_item()
 
